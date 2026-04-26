49a046c036c73aae6f149d1e33e5ea4ac2a6168e
Kateryna <katekirshe95@gmail.com>
2026-04-26 18:17:02 +0300

    reorganize notes components and styles

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -4,7 +4,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import NotesClient from "../../Notes.client";
+import NotesClient from "./Notes.client";
 import { Tag } from "@/types/note";
 
 type Props = {
